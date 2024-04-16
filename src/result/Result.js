2a719ac85cd9a43694b6e0f80641a34e7219c132
David Dean <vermicious.knid.d@gmail.com>
2024-04-16 11:10:40 -0700

    Adds game logic. Adds timer for 1-2-3. Adds result state. Adds computer choice.

--- src/result/Result.js
+++ src/result/Result.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom'
 
 const Result = ({ playerChoice, score, setScore }) => {
   const [ computerChoice, setComputerChoice ] = useState("")
+  const [ result, setResult ] = useState("")
+  const [ counter, setCounter ] = useState(1)
 
   const newComputerPick = () => {
     const choices = [ 'rock', 'paper', 'scissors' ]
@@ -15,6 +17,50 @@ const Result = ({ playerChoice, score, setScore }) => {
     newComputerPick()
   }, [computerChoice])
 
+  const getGameResult = () => {
+    console.log(playerChoice, computerChoice)
+    if (playerChoice === 'rock' && computerChoice === 'scissors') {
+      setResult('win')
+      setScore(score + 1)
+    }
+    else if (playerChoice === 'rock' && computerChoice === 'paper') {
+      setResult('lose')
+      setScore(score - 1)
+    }
+    else if (playerChoice === 'paper' && computerChoice === 'scissors') {
+      setResult('lose')
+      setScore(score - 1)
+    }
+    else if (playerChoice === 'paper' && computerChoice === 'rock') {
+      setResult('win')
+      setScore(score + 1)
+    }
+    else if (playerChoice === 'scissors' && computerChoice === 'paper') {
+      setResult('win')
+      setScore(score + 1)
+    }
+    else if (playerChoice === 'scissors' && computerChoice === 'rock') {
+      setResult('lose')
+      setScore(score - 1)
+    }
+    else {
+      setResult('draw')
+    }
+  }
+
+  useEffect(() => {
+    const timer =
+      counter <= 3
+        ? setInterval(() => {
+          setCounter(counter + 1)
+        }, 1000)
+        : getGameResult()
+
+    return () => {
+      clearInterval(timer)
+    }
+  }, [counter])
+
   // TODO: remove this
   const handleClick = () => {
     console.log("handleClick")
@@ -25,6 +71,7 @@ const Result = ({ playerChoice, score, setScore }) => {
     <div>
       <h1>{playerChoice}</h1>
       <h1>{computerChoice}</h1>
+      <h1>{result}</h1>
       <button onClick={handleClick}>You Win</button>
       <Link to="/">
         Play Again
