59e143d392322c9deb0032bc846102ca8010f8a9
David Dean <vermicious.knid.d@gmail.com>
2024-04-16 10:51:47 -0700

    Ads computer choice on result page load.

--- src/result/Result.js
+++ src/result/Result.js
@@ -1,13 +1,30 @@
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 const Result = ({ playerChoice, score, setScore }) => {
+  const [ computerChoice, setComputerChoice ] = useState("")
+
+  const newComputerPick = () => {
+    const choices = [ 'rock', 'paper', 'scissors' ]
+    const index = Math.floor(Math.random() * 3)
+    console.log(choices, index)
+    setComputerChoice(choices[index])
+  }
+
+  useEffect(() => {
+    newComputerPick()
+  }, [computerChoice])
+
+  // TODO: remove this
   const handleClick = () => {
     console.log("handleClick")
     setScore(score + 1)
   }
+
   return (
     <div>
       <h1>{playerChoice}</h1>
+      <h1>{computerChoice}</h1>
       <button onClick={handleClick}>You Win</button>
       <Link to="/">
         Play Again
