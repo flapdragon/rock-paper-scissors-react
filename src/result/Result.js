72a57f313886cb1da7c266e3195ef0f0fa6d5a61
David Dean <vermicious.knid.d@gmail.com>
2024-04-17 09:15:33 -0700

    Adds route validation to /result.

--- src/result/Result.js
+++ src/result/Result.js
@@ -1,10 +1,20 @@
 import { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 
 const Result = ({ playerChoice, score, setScore }) => {
   const [ computerChoice, setComputerChoice ] = useState("")
   const [ result, setResult ] = useState("")
   const [ counter, setCounter ] = useState(1)
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    // If playerChoice isn't defined or if user came to this route directly
+    if (!playerChoice) {
+      // Redirect to home
+      navigate('/')
+    }
+  }, [])
 
   const newComputerPick = () => {
     const choices = [ 'rock', 'paper', 'scissors' ]
